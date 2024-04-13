e097135acc9aee83a929736953c9113761ea0a4c
Renju Radhakrishnan <renju@block.xyz>
2024-04-12 19:40:11 -0700

    nvim init commit

---
+++ lua/plugins/ayu.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'Shatur/neovim-ayu',
+    config = function()
+      require('ayu').setup({
+        mirage = true,
+      })
+
+      vim.cmd([[colorscheme ayu]])
+    end
+  }
+}
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    requires = 'nvim-lua/plenary.nvim',
+    cmd = "NeoTree",
+  }
+}
