82359e7ffca1965d27774c17c5fa55d391b98de2
Renju Radhakrishnan <renju@block.xyz>
2024-04-12 20:44:20 -0700

    lsp conf

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
+      { "folke/neodev.nvim", opts = {} },
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "gopls", "pylsp"}
+      })
+
+      local on_attach = function(_, _)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+        vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+        vim.keymap.set('n', 'gh', vim.lsp.buf.hover, {})
+      end
+
+      local lspconf = require("lspconfig")
+
+      lspconf.lua_ls.setup {
+        on_attach=on_attach
+      }
+
+      lspconf.gopls.setup {
+        on_attach=on_attach
+      }
+
+
+      lspconf.pylsp.setup {
+        on_attach=on_attach
+      }
+    end
+  }
+}
