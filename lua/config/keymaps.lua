7153d88c889557c526f368064fa270a5dad6dfb9
Renju Radhakrishnan <renju@block.xyz>
2024-04-25 09:40:00 -0700

    persistence

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -33,6 +33,16 @@ function M.setup()
   -- Map the gx key combination to the open_link function
   vim.api.nvim_set_keymap('n', 'gx', ':lua open_link()<CR>', { noremap = true })
 
+  -- Persistence
+  -- restore the session for the current directory
+  vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], {})
+
+  -- restore the last session
+  vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
+
+  -- stop Persistence => session won't be saved on exit
+  vim.api.nvim_set_keymap("n", "<leader>qd", [[<cmd>lua require("persistence").stop()<cr>]], {})
+
 
   vim.keymap.set('n', '<Esc><Esc>', '<cmd>nohlsearch<CR>', { noremap = true, silent = true })
 
