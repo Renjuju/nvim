619aa52b3b1458786816faf8501b6f5c0dcfe31b
Renju Radhakrishnan <renju@block.xyz>
2024-04-25 09:58:43 -0700

    png

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,11 +1,18 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-  }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {
+			filesystem = {
+				bind_to_cwd = true,
+				follow_current_file = { enabled = true },
+				--use_libuv_file_watcher = true,
+			},
+		}
+	}
 }
