792aa78cb60b8800799ae066153e815fadd31ddd
Renju Radhakrishnan <renju@block.xyz>
2024-04-25 21:49:52 -0700

    more stuff

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    }
+}
